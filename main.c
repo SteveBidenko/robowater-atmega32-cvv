4f58f448460eccade681b1665f0e8c42c30f93db
k.panarin@gmail.com <k.panarin@gmail.com@38cc16d9-31f6-b2ff-f3d3-c3b79bdf9571>
2013-08-21 08:02:32 +0000

    Перенос регулярных функций из main.c в regular.c

--- main.c
+++ main.c
@@ -14,9 +14,10 @@
 #include "signals.h"
 #include "keys.h"
 #include "dayofweek.h"
+#include "regular.h"
 // Локальные макроподстановки
 #define MAJOR_VERSION 5
-#define MINOR_VERSION 3
+#define MINOR_VERSION 4
 // #define NODEBUG
 // enum
 // Определение главных структур
@@ -117,13 +118,7 @@ struct st_eeprom_par prim_par={
     }
 };
 unsigned char size_prim_par;        // Почти константа. Нужна для функций записи/чтения из/в EEPROM
-unsigned int time_integration=0;
-unsigned char timer_start_to = 0;
-unsigned char timer_stop_to = 0;
-unsigned int timer_start = 0;
-unsigned char timer_stop = 0;
-unsigned char timer_fan = 0;
-unsigned char count_fan = 0;
+
 int time_cooling = 0;
 int tmp_delta;
 byte tap_angle_min = 0;   // Ограничение крана снизу от температуры
@@ -149,7 +144,7 @@ flash help_str all_help_str[HELP_LINES] = {
 };
 // Описание функций
 void print_help();
-void printallterms(unsigned char); void lcd_primary_screen(void);
+void printallterms(unsigned char);
 void printnewterms(void);
 void print_prim_par(unsigned char *, unsigned char);
 // Функции основного цикла
@@ -165,7 +160,6 @@ void update_P(int);
 // void delay_run(void);
 //void update_PID(int error, int iMin, int iMax);
 void start_screen(unsigned char);
-char high_time_TO(void);        // Функция, проверяющая необходимость проведения ТО
 void full_reset(void);          // функция, востанавливающая все (!) к заводским настройкам
 void factory_reset(void);       // Функция, востанавливающая установки по умолчанию не трогая информацию о термометрах
 void factory_term_reset(void);  // Функция, восстанавливающая настройки из EEPROM и записывающая адреса заводских термометров в EEPROM
@@ -922,52 +916,6 @@ void start_screen(unsigned char init) {
     }
     lcd_puts(linestr);
 }
-
-// Полностью прорисовать главный экран
-void lcd_primary_screen(void) {
-    char run_mod [8];
-    register unsigned char i;
-    unsigned char c_alerts = 0;
-    unsigned char sign = '+';
-    int lcd_term = main_menu[0].val_data;
-    #ifndef NODEBUG
-    // printf ("%02u:%02u Начало прорисовки экрана...", s_dt.cMM, s_dt.cSS);
-    #endif
-    for (i = 0; i < MAX_ALERTS; i++) {
-        // 9-й алерт (отсутствие 3-го термометра) нам не интересен
-        if (prim_par.alert_status[i] && i != 9) c_alerts++;
-    }
-    // Обратите внимание, что значение режима не должно быть длинее 5-ти символов
-    switch (mode.run) {
-        case mo_stop:           sprintf(run_mod, "СТОП "); break;
-        case mo_warming_up:     sprintf(run_mod, "ПРОГР"); break;
-        case mo_warming_down:   sprintf(run_mod, "ОСТАН"); break;
-        case mo_action:         sprintf(run_mod, "ПУСК "); break;
-        case mo_to:             sprintf(run_mod, "ТО   "); break;
-        default: break;
-    };
-    if (c_alerts)
-        sprintf(run_mod, "Авария ");
-    else {
-        signal_red(OFF);  signal_buz(OFF); // signal_green(OFF);
-    }
-    lcd_command(LCD_DISP_ON);       // Убираем курсор с LCD
-    lcd_gotoxy(0,0);        // Устанавливаем курсор в позицию 0 первой строки
-    sprintf(linestr, "%02u:%02u:%02u %02u.%02u %01u", s_dt.cHH, s_dt.cMM, s_dt.cSS, s_dt.cdd, s_dt.cmo, s_dt.dayofweek);
-    lcd_puts(linestr);
-    lcd_gotoxy(0,1);                // Устанавливаем курсор в позицию 0 строки 2
-    // Выводим информацию о термометрах
-    if(ds1820_devices) {
-        sign = (lcd_term < 0) ? '-' : '+';
-        sprintf(linestr, "t=%c%02u.%01uC %s", sign, abs(lcd_term)/100, (abs(lcd_term)%100)/10, run_mod);
-    } else {
-        sprintf(linestr, "Нет термометров");
-    }
-    lcd_puts(linestr);
-    #ifndef NODEBUG
-    // printf ("конец в %02u:%02u\r\n", s_dt.cMM, s_dt.cSS);
-    #endif
-}
 // Печать структуры prim_par
 void print_prim_par(unsigned char *struct_data, unsigned char size) {
     register unsigned char i;
@@ -1098,19 +1046,6 @@ void check_serial(void) {
         };
     }
 }
-// Функция проверяющая условия проведения ТО, и если пора - возвращает ненулевое значение
-char high_time_TO(void) {
-    if (prim_par.TO.weekday == s_dt.dayofweek) {
-        if (prim_par.TO.hour == s_dt.cHH) {
-            if (prim_par.TO.minute == s_dt.cMM) {
-                if (prim_par.TO.status == 0) {
-                    return (1);
-                }
-            }
-        }
-    }
-    return (0);
-}
 // функция, востанавливающая все (!) к заводским настройкам
 void full_reset(void) {
     start_screen(1);
@@ -1145,101 +1080,5 @@ void print_help() {
     for (i = 0; i < HELP_LINES; i++) {
         strcpyf(curr_line, all_help_str[i]);
         printf("%s\r\n", curr_line);
-    }
-}
-// Функция, вызываемая раз в секунду либо по события, либо по прерыванию
-void regular_inspection(void) {
-    #ifndef NODEBUG
-    // printf ("%02u:%02u Начало прерывания Секунда...", s_dt.cMM, s_dt.cSS);
-    // printf (".");
-    #endif
-    if (timer_fan) timer_fan--;
-    if (timer_start) {
-        timer_start--;
-        // Если достигли нуля, то включаем вентилятор
-        if (!timer_start && (mode.run == mo_warming_up)) {         // event = ev_timer_Start;
-             mode.fan = 1;
-    	     #ifndef NODEBUG
-             printf ("ПУСК \r\n");
-	         #endif
-             mode.run = mo_action;  // Включаем режим Пуск
-             count_fan = 0;
-             signal_green(ON);
-        }
-    }
-    if (timer_stop) {
-        timer_stop--;
-        // Если достигли нуля, то выключаем вентилятор
-        if (!timer_stop && (mode.run == mo_warming_down)) {          // event = ev_timer_Stop;
-            mode.run = mo_stop;
-            mode.fan = 0;           // Выключение насоса
-            // printf ("Остановили Вентилятор Режим СТОП \r\n");
-            signal_green(OFF);
-        }
-    }
-     // Пока timer1_counter > 0, уменьшаем его значение
-    if (menu_timer_inactive) {
-        menu_timer_inactive--;
-        // Если достигли нуля, то останавливаем обслуживание valcoder
-        if (!menu_timer_inactive) {
-            if (event)
-                menu_timer_inactive++;
-            else
-                event = ev_timer;
-            // clatsman.valcoder_mode = 0; lcd_clrscr();
-        }
-    }
-    if (!mode.stop_sync_dt) get_cur_dt (0);
-    read_all_terms(DUTY_MODE);
-    // Вычитаваем АЦП
-    ADC_VAR2 = read_adc(0)/4;
-    ADC_VAR1 = read_adc(1)/4;
-    if (time_integration) time_integration--;
-    if (time_cooling ) time_cooling-- ;
-    #ifndef NODEBUG
-    // printf ("конец в %02u:%02u\r\n", s_dt.cMM, s_dt.cSS);
-    #endif
-    s_dt.dayofweek = dayofweek(s_dt.cdd, s_dt.cmo, s_dt.cyy);
-    MAIN_T = read_term(0);      // Выводим информацию о главном термометре !!!
-    POM_T = read_term(0);
-    UL_T = read_term(1);
-    WIN_T = read_term(2);
-    WOUT_T = read_term(3);
-    switch (mode.menu) {
-        case 0: lcd_primary_screen(); break;
-        // case 2: lcd_edit(0); break;
-        case 1: lcd_menu(0); break;
-        default: ;
-    }
-    // Если пора проводить ТО, то генерируем событие ev_begin_to
-    if (high_time_TO()) {
-        printf ("Пора проводить ТО\n");
-        prim_par.TO.status = 1;
-        event = ev_begin_to;
-    } else {
-        event = ev_none;            // Очищаем событие
-    }
-    if (mode.run == mo_to) {
-        if (timer_start_to) {
-            timer_start_to--;
-                // Если достигли нуля, то Событие ev_to_nf
-            if (!timer_start_to && (ADC_VAR1 < prim_par.ADC1_hi)) {
-                printf ("Закончилось время открытия \n");
-                event = ev_to_nf;
-            }
-        }
-        if (timer_stop_to) {
-            timer_stop_to--;
-            if (ADC_VAR1 <= (prim_par.ADC1_lo + 5)) {
-               printf ("Успешное завершение ТО Крана. \n");
-               event = ev_end_to;
-            } else {
-            // Если достигли нуля, то Событие ev_to_nf
-                if (!timer_stop_to && (ADC_VAR1 > (prim_par.ADC1_lo + 10))) {
-                    printf ("Закончилось время закрытия \n");
-                    event = ev_to_nf;
-                }
-            }
-        }
     }
 }
