74b1ecb2d6f6cd2efc6414659c7e944e2acf056d
k.panarin@gmail.com <k.panarin@gmail.com@38cc16d9-31f6-b2ff-f3d3-c3b79bdf9571>
2013-10-01 08:21:53 +0000

    Исправлен баг с постоянным выводом на экран отладочной информации в модуле season.c

--- trunk/season.c
+++ trunk/season.c
@@ -122,10 +122,10 @@ void tap_angle_check_range(void) {
         tap_angle_min = prim_par.tap_angle + ((long int)((TA_IN_NOLIMIT - UL_T) * mode.k_angle_limit)) / 1000;
         printf(" tap_angle_min %u .   Kоэффициент :%d\r\n",  tap_angle_min, mode.k_angle_limit);
     }
-    if (!mode.print) printf("Пересчет ограничения: %d, Ул. т :%d, Коэффициент :%i, ограничение снизу :%d, UL_T :%i  \r\n",  (TA_IN_NOLIMIT - UL_T), UL_T, mode.k_angle_limit,prim_par.tap_angle, UL_T);
+    if (mode.print) printf("Пересчет ограничения: %d, Ул. т :%d, Коэффициент :%i, ограничение снизу :%d, UL_T :%i  \r\n",  (TA_IN_NOLIMIT - UL_T), UL_T, mode.k_angle_limit,prim_par.tap_angle, UL_T);
     if (TAP_ANGLE < tap_angle_min)
         TAP_ANGLE = tap_angle_min;
     else
         if (TAP_ANGLE > PWM_MAX) TAP_ANGLE = PWM_MAX;
-    printf(" Установка крана %u . prim_par.tap_angle :%d\r\n", TAP_ANGLE, prim_par.tap_angle);
+    if (mode.print) printf(" Установка крана %u . prim_par.tap_angle :%d\r\n", TAP_ANGLE, prim_par.tap_angle);
 }
